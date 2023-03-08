508e9110eab7d55d00b9231f0f614850d38f2746
Damian Szyszka <damianszyszka16@gmail.com>
2023-03-08 09:27:52 +0100

    brake

--- main/index.php
+++ main/index.php
@@ -4,9 +4,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Udane Wakacje</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Okna na miarę</title>
 </head>
 <body>
-    
+    <header>
+        <div class="logo">
+            <img src="../images/logo.png" alt="">
+        </div>
+        <div class="menu">
+            <ul>
+                <li>o nas</li>
+                <li>oferta</li>
+                <li>kontakt</li>
+            </ul>
+        </div>
+    </header>
 </body>
 </html>
